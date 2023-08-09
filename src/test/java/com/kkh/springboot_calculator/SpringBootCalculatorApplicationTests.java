2cfa42066240629057f4694d8f740445d6f2529e
KKH <kkh30123@naver.com>
2023-08-10 02:15:33 +0900

    newfiles_add

---
+++ src/test/java/com/kkh/springboot_calculator/SpringBootCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkh.springboot_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
